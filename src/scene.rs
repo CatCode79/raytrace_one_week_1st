5b68abdec8d416e411076165923cd19b80681e80
CatCode79 <andrea.postal@gmail.com>
2024-07-09 23:56:22 +0200

    blu to white gradient

--- src/scene.rs
+++ src/scene.rs
@@ -1,6 +1,6 @@
 //= IMPORTS ==================================================================
 
-use glam::Vec3;
+use glam::{dvec3, DVec3, uvec3, Vec3, vec3};
 
 //= SCENE ====================================================================
 
@@ -14,31 +14,65 @@ impl Scene {
     pub(crate) fn compute(&mut self) {
         profiling::scope!("render");
 
+        // Camera
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (self.width as f64 / self.height as f64);
+        let camera_center = dvec3(0.0, 0.0, 0.0);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        let viewport_u = dvec3(viewport_width, 0.0, 0.0);
+        let viewport_v = dvec3(0.0, -viewport_height, 0.0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        let pixel_delta_u = viewport_u / self.width as f64;
+        let pixel_delta_v = viewport_v / self.height as f64;
+
+        // Calculate the location of the upper left pixel.
+        let viewport_upper_left = camera_center
+            - dvec3(0.0, 0.0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
         for (i, pixel) in self.data.iter_mut().enumerate() {
             let width = i % self.width as usize;
             let height = i / self.width as usize;
 
-            let r = width as f32 / self.width as f32;
-            let g = height as f32 / self.height as f32;
+            let pixel_center = pixel00_loc + (width as f64 * pixel_delta_u) + (height as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let ray = Ray::new(camera_center, ray_direction);
 
-            *pixel = u32::from_ne_bytes([(255.9999 * r) as u8, (255.9999 * g) as u8, 0_u8, 255_u8]);
+            let color = ray.color();
+            *pixel = u32::from_ne_bytes([
+                (255.9999 * color.x) as u8,
+                (255.9999 * color.y) as u8,
+                (255.9999 * color.z) as u8,
+                255_u8]);
         }
     }
 }
 
 //= COLOR ====================================================================
 
-type Color = Vec3;
+type Color = DVec3;
 
 //= RAY ======================================================================
 
 struct Ray {
-    pub origin: Vec3,
-    pub direction: Vec3,
+    pub origin: DVec3,
+    pub direction: DVec3,
 }
 
 impl Ray {
-    fn ray_color(ray: &Ray) -> Color {
-        return Color::new(0.0, 0.0, 0.0);
+    fn new(origin: DVec3, direction: DVec3) -> Self {
+        Self {
+            origin,
+            direction,
+        }
+    }
+
+    fn color(self: &Ray) -> Color {
+        let unit_direction = self.direction.normalize();
+        let a = 0.5 * (unit_direction.y + 1.0);
+        return (1.0 - a) * Color::new(1.0, 1.0, 1.0) + (a * Color::new(0.5, 0.7, 1.0));
     }
 }
